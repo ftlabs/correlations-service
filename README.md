d95407d165d176a6d224cee16d2a5b8f4a01d5cc
Chris Gathercole <chris.gathercole@ft.com>
2017-06-05 13:48:57 +0100

    first commit, barebones express app with dummy endpoints and some possibly useful code from another project

---
+++ README.md
@@ -0,0 +1,27 @@
+# FT Labs rebuild of Slurp, surfacing correlations between people and orgs
+
+Basic gist:
+
+* read info from SAPI
+* build in-memory indexes
+* support six degrees -esque functionality
+
+# new end points
+
+* /...
+
+# Environment Parameters
+
+When building locally, specify them in a local file, .env (and NB, this must not be included in the git repo, hence has a specific line in .gitignore). When deploying to Heroku, they need to be specified in the app's settings, Config Variables.
+
+The following are mandatory params, the absence of which will kill the app on startup:
+
+* TOKEN=...
+* CAPI_KEY=...
+
+These are for local builds:
+
+* SERVER_ROOT=TRANSPORT_AND_DOMAIN_OF_SERVICE
+   * to use when constructing the podcast feed
+   * e.g. when developing locally, http://localhost:8060
+* DEBUG=correlations:\*,bin:lib:\*
---
+++ index.js
@@ -0,0 +1,48 @@
+const  dotenv = require('dotenv').config();
+const   debug = require('debug')('autovoice:index');
+const express = require('express');
+const    path = require('path');
+const     app = express();
+
+// const               autovoice = require('./bin/lib/autovoice');
+
+const authS3O = require('s3o-middleware');
+
+var requestLogger = function(req, res, next) {
+    debug("RECEIVED REQUEST:", req.method, req.url);
+    next(); // Passing the request to the next handler in the stack.
+}
+
+app.use(requestLogger);
+
+// these routes do *not* have s3o
+
+app.use('/static', express.static('static'));
+
+app.get('/__gtg', (req, res) => {
+	res.status(200).end();
+});
+
+const TOKEN = process.env.TOKEN;
+if (! TOKEN ) {
+  throw new Error('ERROR: TOKEN not specified in env');
+}
+
+app.get('/dummy', (req, res) => {
+  res.json({ testing: "testing", 'one-two-three' : 'testing' });
+});
+
+// these route *do* use s3o
+app.set('json spaces', 2);
+
+app.use(authS3O);
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/static/index.html'));
+});
+
+//---
+
+app.listen(process.env.PORT, function(){
+	debug('Server is listening on port', process.env.PORT);
+});
