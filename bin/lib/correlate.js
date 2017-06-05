bd85f883360f32fe830a9bab29b2d6051780984b
Chris G <cgathercole@gmail.com>
2017-06-05 22:35:19 +0100

    first step of updateCorrelations, getting all entities from last time interval

--- bin/lib/correlate.js
+++ bin/lib/correlate.js
@@ -13,7 +13,39 @@ const knownEntities = {
 
 function updateCorrelations(afterSecs, beforeSecs) {
 	return fetchContent.searchUnixTimeRange(afterSecs, beforeSecs)
-		.then( sapiObj => { return knownEntities; })
+		.then( sapiObj => {
+			const deltaEntities = {};
+			if (! sapiObj.results ) {
+				debug('updateCorrelations: no results');
+			} else if( ! sapiObj.results[0] ) {
+				debug('updateCorrelations: no results[0]');
+			} else if( ! sapiObj.results[0].facets ) {
+				debug('updateCorrelations: no results[0].facets');
+			} else {
+				sapiObj.results[0].facets.forEach( facet => {
+					const ontology = facet.name;
+					if (! knownEntities.hasOwnProperty(ontology)) {
+						knownEntities[ontology] = {};
+					}
+					deltaEntities[ontology] = {};
+					const ontologyEntities = knownEntities[ontology];
+					facet.facetElements.forEach( element => {
+						const entity = `${ontology}:${element.name}`;
+						if (! ontologyEntities.hasOwnProperty(entity)) {
+							ontologyEntities[entity] = 0;
+						}
+						ontologyEntities[entity] = ontologyEntities[entity] + element.count;
+						deltaEntities[ontology][entity] = element.count;
+					});
+				});
+			}
+			return deltaEntities
+		})
+		// .then( ) // loop over each new entity, to get all facets
+		// .then( ) // loop over each new pair of entities, to get all titles
+		// .then( ) // iterate over pairs of entities to find connected islands
+		// .then( ) // iterate over each island to find merkel chains
+		// .then( ) // update main records
 		;
 }
 
