4532649d20d8fbcc1f257e9b60d0f4642a83ab9f
Sean Tracey <seanmtracey@gmail.com>
2017-07-06 15:28:05 +0100

    Let S3O handle auth failures.

--- bin/lib/check-token.js
+++ bin/lib/check-token.js
@@ -6,23 +6,11 @@ module.exports = (req, res, next) => {
 
 	debug(`Checking if token '${passedToken}' is valid`);
 
-	if(!passedToken){
-		debug(`'token' was not passed for request. Value is '${passedToken}'`);
-		res.status(422);
-		res.json({
-			status : 'error',
-			message : `A valid token must be passed with the 'token' query parameter to access this resource`
-		});
-	} else if(passedToken === process.env.USER_ACCESS_TOKEN){
+	if(passedToken === process.env.USER_ACCESS_TOKEN){
 		debug(`Token '${passedToken}' was valid`);
 		next('route');
 	} else {
-		debug(`Token '${passedToken}' is not valid`);
-		res.status(401);
-		res.json({
-			status : 'err',
-			message : 'The token passed was not valid'
-		});
+		next();
 	}
 
 }
