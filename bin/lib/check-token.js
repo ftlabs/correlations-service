bc827e48ebea2a7aebc9257c5f5dd7085d4f122f
Chris Gathercole <chris.gathercole@ft.com>
2017-07-06 17:05:18 +0100

    stop writing token to log

--- bin/lib/check-token.js
+++ bin/lib/check-token.js
@@ -5,13 +5,13 @@ module.exports = (req, res, next) => {
 
 	const passedToken = req.query.token;
 
-	debug(`Checking if token '${passedToken}' is valid`);
+	debug(`Checking if token is valid`);
 
 	if(passedToken === undefined){
 		debug(`No token has been passed to service. Falling through to S3O`);
 		S3O(req, res, next);
 	} else if(passedToken === process.env.TOKEN){
-		debug(`Token '${passedToken}' was valid`);
+		debug(`Token was valid`);
 		next();
 	} else {
 		res.status(401);
