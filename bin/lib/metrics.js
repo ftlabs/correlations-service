80d1680a1b3d519b92598afb09001bf2711b0c3a
Alexander.Marzan <alexander.marzan@FT-MW4403.local>
2018-07-24 11:21:12 +0100

    Pushing required  metrics to graphite

--- bin/lib/metrics.js
+++ bin/lib/metrics.js
@@ -4,7 +4,7 @@ const correlate = require('./correlate');
 
 
 var socket = net.createConnection(2003, "graphite.ft.com", function() {
-	console.log("Connection created");
+	//console.log("Connection created");
 
 });
 
@@ -30,15 +30,15 @@ function send_post_metrics() {
 		metric_line = metric_line + "demo.ft-lab.correlations-service." + correlate.ontology() + ".post." + filtered[i] + " " + parseFloat(postObj[filtered[i]]) + " " + Math.floor(Date.now() / 1000) + "\n";
 	}
 
-	console.log(metric_line);
+	//console.log(metric_line);
 
 
 	const writeResponse = socket.write(metric_line, function () {
-			console.log(`Write callback metric line = ${metric_line}` );
+			//console.log(`Write callback metric line = ${metric_line}` );
 		});
 
 
-	console.log(`writeResponse =${writeResponse}`);
+	//console.log(`writeResponse =${writeResponse}`);
 }
 
 
@@ -55,26 +55,46 @@ function send_get_metrics() {
 	var metric_line = "";
 
 	for(var i=0; i < filtered.length; i++) {
-		metric_line = metric_line + "demo.ft-lab.correlations-service."+correlate.ontology()+".get." + filtered[i] + " " + parseFloat(postObj[filtered[i]]) + " " + Math.floor(Date.now() / 1000) + "\n";
+		metric_line = metric_line + "demo.ft-lab.correlations-service." + correlate.ontology() + ".get." + filtered[i] + " " + parseFloat(postObj[filtered[i]]) + " " + Math.floor(Date.now() / 1000) + "\n";
 	}
 
-	console.log(metric_line);
+	//console.log(metric_line);
 
 
 	const writeResponse = socket.write(metric_line, function () {
-			console.log(`Write callback metric line = ${metric_line}` );
+			//console.log(`Write callback metric line = ${metric_line}` );
 		});
 
 
-	console.log(`writeResponse =${writeResponse}`);
+	//console.log(`writeResponse =${writeResponse}`);
 }
 
 
 function test() {
 	const data=correlate.summary();
 	const postObj = data["counts"];
-	console.log("TEST: " + postObj["knownEntities"]);
-	console.log(correlate.summary());
+	const intervalHrs = data["times"]["intervalCoveredHrs"];
+	//console.log(correlate.summary());
+
+	counts_items = Object.keys(postObj);
+
+	
+	var interval_metric = "demo.ft-lab.correlations-service."	+ correlate.ontology() + ".times.intervalCoveredHrs " + parseFloat(intervalHrs) + " " + Math.floor(Date.now() / 1000) + "\n";
+	var metric_line = interval_metric;
+
+	for( i in counts_items) {
+		metric_line = metric_line + "demo.ft-lab.correlations-service."	+ correlate.ontology() + ".counts." + counts_items[i] + " " + parseFloat(postObj[counts_items[i]]) + " " + Math.floor(Date.now() / 1000) + "\n";
+		//console.log(counts_items[i] + ": " + postObj[counts_items[i]]);
+	}
+
+	console.log(metric_line);
+	const writeResponse = socket.write(metric_line, function () {
+		//console.log(`Write callback metric line = ${metric_line}` );
+	});
+
+
+
+	
 }
 
 
